3a0b9ce785f7f0702fed1344d7e7663eaebc1762
Orlando G. Rodriguez <orlandogrodriguez94@gmail.com>
2018-01-07 19:20:12 -0500

    Started giving the app some structure. Attempted to create the views for the thresholds. Still working on this.

--- iOS/Swayter/Swayter/HomeViewController.swift
+++ iOS/Swayter/Swayter/HomeViewController.swift
@@ -8,10 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
+    var thresholds = [TemperatureThreshold]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Dummy temperature thresholds for testing:
+        fetchThresholds(uid: "12345")
+        
+    }
+    
+    func fetchThresholds(uid: String) {
+        //TODO: get thresholds from firebase and add to thresholds array.
+        fetchTestThresholds()   //Remove this upon completing firebase implementation.
+    }
+    
+    func fetchTestThresholds() {
+        var threshold0 = TemperatureThreshold.init(name: "Sweater", temperature: 68)
+        var threshold1 = TemperatureThreshold.init(name: "Jacket", temperature: 57)
+        var threshold2 = TemperatureThreshold.init(name: "Winter Jacket", temperature: 50)
+        thresholds.append(threshold0)
+        thresholds.append(threshold1)
+        thresholds.append(threshold2)
     }
 }
